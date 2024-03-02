e2f2ff4d5519b4e066060354c6a536a2c56e499c
Anat3r <ruslanbedychevwork@gmail.com>
2024-03-02 21:32:38 +0100

    change error to display warning

--- js/app.js
+++ js/app.js
@@ -75,7 +75,7 @@ class Calculator {
   calculate(str) {
     let exp = str.split(" ").filter((el) => el !== " ");
     exp.forEach(el => {
-      if (!(Number.isFinite(+el) || this.methods[el])) document.getElementById("display").innerText = "NaN";
+      if (!(Number.isFinite(+el) || this.methods[el])) return "NaN";
     });
     let ops = exp.filter((el) => this.methods[el]);
     let sort_ops = Array.from(ops);
