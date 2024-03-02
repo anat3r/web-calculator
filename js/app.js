25e68a53ed0d24f42580bb68a1a6c004b4d71922
Anat3r <ruslanbedychevwork@gmail.com>
2024-03-02 21:19:16 +0100

    delete deleteAllCookies function

--- js/app.js
+++ js/app.js
@@ -1,15 +1,5 @@
 "use strict";
 
-(function deleteAllCookies() {
-  const cookies = document.cookie.split(";");
-
-  for (let i = 0; i < cookies.length; i++) {
-    const cookie = cookies[i];
-    const eqPos = cookie.indexOf("=");
-    const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-  }
-})();
 Array.from(document.getElementsByTagName('a')).forEach((el) => {
   el.id = el.innerText.toLowerCase()
   el.onclick = () => calculator.input(el);
